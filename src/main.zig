590e6253a8f493b1a6dc59113252c803831f3845
Philipp Wendel <philipp@wendel.io>
2026-02-07 14:53:33 +0100

    refactor args from struct to array

--- src/main.zig
+++ src/main.zig
@@ -6,22 +6,24 @@ const Ast = zigableiter.Ast(f32);
 pub fn main() !void {
     std.debug.print("Simple zigableiter demo\n", .{});
 
-    const function = Ast{ .add = &.{ .l = Ast{ .variable = 1.0 }, .r = Ast{ .variable = 4.0 } } };
+    const function = Ast{ .add = .{ &Ast.@"1", &Ast.@"1" } };
     std.debug.print("Num: {d}\n", .{comptime function.eval().?});
 
     const base = 2;
     std.debug.print("Derive {d}^n for n = 0 to 10\n", .{base});
+
     inline for (0..10) |i| {
         const exp = @as(f32, @floatFromInt(i));
-        const pow = Ast{ .pow = &.{ .l = .{ .constant = base }, .r = .{ .constant = exp } } };
+        const pow = Ast{ .pow = .{ &.{ .constant = base }, &.{ .constant = exp } } };
         const pow_d = comptime pow.derive().?;
-        std.debug.print("{d}: {d}^{d}={d}; Auto Derived: {d}; Hand Derived: {d}\n", .{ i, pow.pow.l.constant, pow.pow.r.constant, pow.eval().?, pow_d.eval().?, exp * std.math.pow(f32, base, exp - 1) });
+        std.debug.print("{d}: {d}^{d}={d}; Auto Derived: {d}; Hand Derived: {d}\n", .{ i, pow.pow[0].constant, pow.pow[1].constant, pow.eval().?, pow_d.eval().?, exp * std.math.pow(f32, base, exp - 1) });
         pow_d.print();
     }
-    // std.debug.print("Derive {d}^n for n = -10 to 0\n", .{base});
-    // inline for (0..10) |i| {
-    //     const pow = Ast{ .pow = &.{ .l = .{ .constant = base }, .r = .{ .constant = (-1) * @as(f32, @floatFromInt(i)) } } };
-    //     const pow_d = comptime pow.derive().?;
-    //     std.debug.print("{d}: {d}^{d}= {d}\n", .{ i, pow.pow.l.constant, pow.pow.r.constant, pow_d.eval().? });
-    // }
+
+    std.debug.print("Derive {d}^n for n = -10 to 0\n", .{base});
+    inline for (0..10) |i| {
+        const pow = Ast{ .pow = .{ &.{ .constant = base }, &.{ .constant = (-1) * @as(f32, @floatFromInt(i)) } } };
+        const pow_d = comptime pow.derive().?;
+        std.debug.print("{d}: {d}^{d}= {d}\n", .{ i, pow.pow[0].constant, pow.pow[1].constant, pow_d.eval().? });
+    }
 }
--- src/root.zig
+++ src/root.zig
@@ -3,60 +3,66 @@ const std = @import("std");
 pub fn Ast(T: type) type {
     return union(enum) {
         const Self = @This();
-        pub const ARGS = []Self;
-        pub const LR = struct { l: Self, r: Self };
-        add: *const LR,
-        sub: *const LR,
-        mul: *const LR,
-        div: *const LR,
-        pow: *const LR,
-        sin: *const Self,
-        cos: *const Self,
+        pub fn Args(n: usize) type {
+            return [n]*const Self;
+        }
+
+        add: Args(2),
+        sub: Args(2),
+        mul: Args(2),
+        div: Args(2),
+        pow: Args(2),
+        sin: Args(1),
+        cos: Args(1),
         variable: T,
         constant: T,
 
-        fn apply(f: fn (T, T) T, node: LR) ?T {
-            const l = node.l.eval() orelse return null;
-            const r = node.r.eval() orelse return null;
+        pub const @"1" = Self{ .constant = 1 };
+        pub const @"-1" = Self{ .constant = -1 };
+
+        fn apply1(f: fn (T) T, args: Args(1)) ?T {
+            const l = args[0].eval() orelse return null;
+            return f(l);
+        }
+        fn apply2(f: fn (T, T) T, args: Args(2)) ?T {
+            const l = args[0].eval() orelse return null;
+            const r = args[1].eval() orelse return null;
             return f(l, r);
         }
 
         pub fn derive(ast: Self) ?Self {
             return switch (ast) {
                 .constant => null,
-                .variable => .{ .constant = 1 },
-                .pow => |lr| .{
-                    .mul = &.{
-                        .l = lr.r,
-                        .r = .{
-                            .pow = &.{
-                                .l = lr.l,
-                                .r = .{
-                                    .sub = &.{
-                                        .l = lr.r,
-                                        .r = .{ .constant = 1 },
-                                    },
-                                },
-                            },
+                .variable => Self.@"1",
+                .pow => |args| power_rule(args),
+                .sin => |x| .{ .cos = x },
+                .cos => |x| .{ .mul = .{ &Self.@"-1", &.{ .sin = x } } },
+                else => ast,
+            };
+        }
+        fn power_rule(args: Args(2)) Self {
+            return .{
+                .mul = .{
+                    args[1],
+                    &.{
+                        .pow = .{
+                            args[0],
+                            &.{ .sub = .{ args[1], &Self.@"1" } },
                         },
                     },
                 },
-                .sin => |x| .{ .cos = x },
-                .cos => |x| .{ .mul = &.{ .l = .{ .constant = (-1) }, .r = .{ .sin = x } } },
-                else => ast,
             };
         }
         pub fn eval(ast: Self) ?T {
             return switch (ast) {
-                .add => |node| apply(fun.add, node.*),
-                .sub => |node| apply(fun.sub, node.*),
-                .mul => |node| apply(fun.mul, node.*),
-                .div => |node| apply(fun.div, node.*),
-                .pow => |node| apply(fun.pow, node.*),
-                .sin => |x| std.math.sin(x.eval() orelse return null),
-                .cos => |x| std.math.cos(x.eval() orelse return null),
-                .constant => |constant| constant,
-                .variable => |value| value,
+                .add => |node| apply2(fun.add, node),
+                .sub => |node| apply2(fun.sub, node),
+                .mul => |node| apply2(fun.mul, node),
+                .div => |node| apply2(fun.div, node),
+                .pow => |node| apply2(fun.pow, node),
+                .sin => |node| apply1(fun.sin, node),
+                .cos => |node| apply1(fun.cos, node),
+                .variable, .constant => |value| value,
             };
         }
 
@@ -76,6 +82,12 @@ pub fn Ast(T: type) type {
             pub fn pow(a: T, b: T) T {
                 return std.math.pow(T, a, b);
             }
+            pub fn sin(a: T) T {
+                return std.math.sin(a);
+            }
+            pub fn cos(a: T) T {
+                return std.math.cos(a);
+            }
         };
         pub fn print(self: Self) void {
             self.printIndented(0);
@@ -86,12 +98,11 @@ pub fn Ast(T: type) type {
             switch (self) {
                 .add, .sub, .mul, .div, .pow => |v| {
                     std.debug.print("{s}\n", .{@tagName(self)});
-                    v.l.printIndented(indent_new);
-                    v.r.printIndented(indent_new);
+                    for (v) |s| s.printIndented(indent_new);
                 },
                 .sin, .cos => |v| {
                     std.debug.print("{s}\n", .{@tagName(self)});
-                    v.printIndented(indent_new);
+                    for (v) |s| s.printIndented(indent_new);
                 },
                 .variable, .constant => |num| std.debug.print("{d}\n", .{num}),
             }
@@ -102,13 +113,13 @@ pub fn Ast(T: type) type {
 const TestAst = Ast(f32);
 
 test "Derivation of a constant is 0" {
-    const constant = TestAst{ .constant = 1.0 };
+    const constant = TestAst.@"1";
     try std.testing.expectEqual(constant.derive(), null);
 }
 
 test "Derivation of variable (e.g. x) is 1" {
-    const variable = TestAst{ .variable = 1.0 };
-    try std.testing.expectEqualDeep(variable.derive().?, TestAst{ .constant = 1.0 });
+    const variable = TestAst{ .variable = 2.0 };
+    try std.testing.expectEqualDeep(variable.derive().?, TestAst.@"1");
 }
 
 test "Power rule" {
@@ -128,26 +139,23 @@ test "Power rule" {
     };
     inline for (data) |d| {
         const exp = d.exp;
-        const pow = TestAst{ .pow = &.{ .l = .{ .variable = d.base }, .r = .{ .variable = exp } } };
+        const pow = TestAst{ .pow = .{ &.{ .variable = d.base }, &.{ .variable = exp } } };
         const pow_d = comptime pow.derive().?;
         const res = pow_d.eval().?;
-        // std.debug.print("{d}^{d} -> expected: {d}, actual: {d}\n", .{ d.base, d.exp, d.derived, res });
-        try std.testing.expectApproxEqAbs(res, d.derived, 0.00000001);
+        try std.testing.expectApproxEqAbs(d.derived, res, 0.00000001);
     }
 }
 
 test "derive sin to cos" {
-    const sin = TestAst{ .sin = &.{ .constant = 1 } };
-    const cos = TestAst{ .cos = &.{ .constant = 1 } };
+    const sin = TestAst{ .sin = .{&TestAst.@"1"} };
+    const cos = TestAst{ .cos = .{&TestAst.@"1"} };
     try std.testing.expectApproxEqAbs(sin.derive().?.eval().?, cos.eval().?, 0.0000001);
 }
 
 test "derive cos to -sin" {
-    const cos = TestAst{ .cos = &.{ .constant = 1 } };
+    const x = TestAst.Args(1){&TestAst.@"1"};
+    const cos = TestAst{ .cos = x };
     const cos_d = comptime cos.derive().?;
-    const minus_sin = TestAst{ .mul = &.{
-        .l = .{ .constant = -1 },
-        .r = .{ .sin = &.{ .constant = 1 } },
-    } };
+    const minus_sin = TestAst{ .mul = .{ &TestAst.@"-1", &.{ .sin = x } } };
     try std.testing.expectApproxEqAbs(cos_d.eval().?, minus_sin.eval().?, 0.0000001);
 }
