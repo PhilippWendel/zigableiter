6fa2a155127806f8f6506196b04952b6743c0682
Philipp Wendel <philipp@wendel.io>
2026-02-07 08:01:06 +0100

    Got simple stuff like deriving constants, variables and basic power rule to work

--- src/main.zig
+++ src/main.zig
@@ -6,6 +6,19 @@ const Ast = zigableiter.Ast(f32);
 pub fn main() !void {
     std.debug.print("Simple zigableiter demo\n", .{});
 
-    const lr = Ast.LR{ .l = Ast{ .val = 1.0 }, .r = Ast{ .val = 4.0 } };
-    std.debug.print("Num: {d}\n", .{comptime (Ast{ .add = &lr }).eval().?});
+    const function = Ast{ .add = &.{ .l = Ast{ .variable = 1.0 }, .r = Ast{ .variable = 4.0 } } };
+    std.debug.print("Num: {d}\n", .{comptime function.eval().?});
+
+    std.debug.print("Derive 1^n for n = 0 to 10", .{});
+    inline for (0..10) |i| {
+        const pow = Ast{ .pow = &.{ .l = .{ .variable = 1 }, .r = .{ .variable = @as(f32, @floatFromInt(i)) } } };
+        const pow_d = comptime pow.derive().?;
+        std.debug.print("{d}: {d}^{d}= {d}\n", .{ i, pow.pow.l.variable, pow.pow.r.variable, pow_d.eval().? });
+    }
+    std.debug.print("Derive 1^n for n = -10 to 0", .{});
+    inline for (0..10) |i| {
+        const pow = Ast{ .pow = &.{ .l = .{ .variable = 1 }, .r = .{ .variable = (-1) * @as(f32, @floatFromInt(i)) } } };
+        const pow_d = comptime pow.derive().?;
+        std.debug.print("{d}: {d}^{d}= {d}\n", .{ i, pow.pow.l.variable, pow.pow.r.variable, pow_d.eval().? });
+    }
 }
--- src/root.zig
+++ src/root.zig
@@ -9,7 +9,10 @@ pub fn Ast(T: type) type {
         mul: *const LR,
         div: *const LR,
         pow: *const LR,
-        val: T,
+        sin: Self,
+        cos: Self,
+        variable: T,
+        constant: T,
 
         fn apply(f: fn (T, T) T, node: LR) ?T {
             const l = node.l.eval() orelse return null;
@@ -17,6 +20,29 @@ pub fn Ast(T: type) type {
             return f(l, r);
         }
 
+        pub fn derive(ast: Self) ?Self {
+            return switch (ast) {
+                .constant => null,
+                .variable => .{ .constant = 1 },
+                .pow => |lr| .{
+                    .mul = &.{
+                        .l = lr.r,
+                        .r = .{
+                            .pow = &.{
+                                .l = lr.l,
+                                .r = .{
+                                    .sub = &.{
+                                        .l = lr.r,
+                                        .r = .{ .constant = 1 },
+                                    },
+                                },
+                            },
+                        },
+                    },
+                },
+                else => ast,
+            };
+        }
         pub fn eval(ast: Self) ?T {
             return switch (ast) {
                 .add => |node| apply(fun.add, node.*),
@@ -24,7 +50,8 @@ pub fn Ast(T: type) type {
                 .mul => |node| apply(fun.mul, node.*),
                 .div => |node| apply(fun.div, node.*),
                 .pow => |node| apply(fun.pow, node.*),
-                .val => |val| val,
+                .constant => |constant| constant,
+                .variable => |value| value,
             };
         }
 
@@ -46,4 +73,22 @@ pub fn Ast(T: type) type {
             }
         };
     };
+}
+
+const TestAst = Ast(f32);
+
+test "Derivation of a constant is 0" {
+    const constant = TestAst{ .constant = 1.0 };
+    try std.testing.expectEqual(constant.derive(), null);
+}
+
+test "Derivation of variable (e.g. x) is 1" {
+    const variable = TestAst{ .variable = 1.0 };
+    try std.testing.expectEqualDeep(variable.derive().?, TestAst{ .constant = 1.0 });
+}
+
+test "Power rule" {
+    const pow = TestAst{ .pow = &.{ .l = .{ .variable = 1 }, .r = .{ .variable = 2 } } };
+    const pow_d = pow.derive().?;
+    try std.testing.expectApproxEqAbs(pow_d.eval().?, 2.0, 0.00000001);
 }
